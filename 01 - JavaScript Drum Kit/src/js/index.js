2fd123eede6c1b72ea3d1c78df66ae2a98858c93
zsolt-fekete <zsoltfekete.zsf@gmail.com>
2017-03-15 01:09:39 +0100

    Final commit

--- 01 - JavaScript Drum Kit/src/js/index.js
+++ 01 - JavaScript Drum Kit/src/js/index.js
@@ -1,6 +1,6 @@
 const LIST = Array.from(document.getElementsByClassName("key")).map(element => parseInt(element.dataset.key));
 
-validatePush = (event) => {LIST.includes(event.which) ? acceptedPush(event.which) : null};
+validatePush = (event) => LIST.includes(event.which) ? acceptedPush(event.which) : null;
 
 acceptedPush = (target) => {
   let element = Array.from(document.querySelectorAll('[data-key="' + target +'"]'));
@@ -8,16 +8,13 @@ acceptedPush = (target) => {
   playSound(element[1]);
 }
 
-displayAnimation = (element) => {element.classList.add('playing')}
+displayAnimation = (element) => element.classList.add('playing');
 
 playSound = (audio) => {
   audio.currentTime = 0;
   audio.play();
 }
 
-removeTransition = (element) => {
-  element.classList.remove('playing');
-}
-
+removeTransition = (element) => (element.propertyName !== 'transform') ? null : element.currentTarget.classList.remove('playing');
+document.querySelectorAll('.key').forEach(key => key.addEventListener('transitionend',removeTransition));
 window.addEventListener('keydown', validatePush);
-window.addEventListener('transitionend', removeTransition);
